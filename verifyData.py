a699c22dac31d4c7eef4d02343f98b245cdf9300
1921233 <83212565+1921233@users.noreply.github.com>
2024-04-11 16:49:43 +0200

    clean up + comments

--- verifyData.py
+++ verifyData.py
@@ -1,39 +1,42 @@
 
 from Bio import SeqIO
 from Bio.SeqUtils import CheckSum
-import itertools
 
 
-def verifyData(records):
+def verifyData(records) -> dict:
     removedRecords = []
-    modThree = []
-    doubleData = []
-    stopCodon = []
     checkSum = []
-    oldLength = len(records)
+    reportData = {
+        'modThree': 0,
+        'stopCodon':0,
+        'doubleData': 0,
+        'oldLength': len(records)
+        }
     for val in records.values():
         seq = val.seq
         key = CheckSum.seguid(seq)
+        #Überprüft ob Sequenz durch drei Teilbar
         if(len(seq)%3 != 0):
             removedRecords.append(val.id)
-            modThree.append(val.id)
-        if(key in checkSum):
-            removedRecords.append(val.id)
-            doubleData.append(val.id)
+            reportData['modThree'] +=1
+        #Überprüft ob Stop Codon nicht nur am Ende steht
         if('*' in seq.translate()[:-1]):
             removedRecords.append(val.id)
-            stopCodon.append(val.id)
+            reportData['stopCodon'] +=1
+        #Prüft anhand des "SEquence Globally Unique IDentifier" ob Sequenz bereits da ist.
+        if(key in checkSum):
+            removedRecords.append(val.id)
+            reportData['doubleData'] +=1
         else:
             checkSum.append(key)
-
-    for key in itertools.chain(modThree,doubleData,stopCodon):
+    #Entfernt alle fehlerhaften Datensätze
+    for key in removedRecords:
         del records[key]
-    return {"records": records,"modThree":modThree,"doubleData": doubleData,"stopCodon": stopCodon, "oldLength": oldLength}
+    return {"records": records,"report":reportData}
 
 records = SeqIO.to_dict(SeqIO.parse("data\E.Coli\GCA_000005845.2\cds_from_genomic.fna", "fasta"))
 
 cleanRecords = verifyData(records=records)
-print(len(cleanRecords["records"])/cleanRecords["oldLength"])
-print(len(cleanRecords["modThree"])/cleanRecords["oldLength"])
-print(len(cleanRecords["doubleData"])/cleanRecords["oldLength"])
-print(len(cleanRecords["stopCodon"])/cleanRecords["oldLength"])
+print(cleanRecords['report'])
+print(len(cleanRecords['records']))
+
